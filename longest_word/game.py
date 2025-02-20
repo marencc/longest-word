e6ec1f96b60450ec6810f566035025076a88fb74
Maren Calleja <me.callejac@gmail.com>
2025-02-20 18:46:11 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,5 +1,7 @@
 import random
 import string
+import requests
+
 # pylint: disable=missing-docstring
 # pylint: disable=too-few-public-methods
 class Game:
@@ -16,4 +18,11 @@ class Game:
                 letters.remove(letter)
             else:
                 return False
-        return True
+        return self.__check_dictionary(word)
+
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://dictionary.lewagon.com/{word}")
+        json_response = response.json()
+        return json_response['found']
--- tests/test_game.py
+++ tests/test_game.py
@@ -15,8 +15,9 @@ class TestGame:
             assert len(grid) == 9
             for letter in grid:
                 assert letter in string.ascii_uppercase
-        def test_empty_word_is_invalid(self):
-        # setup
+
+    def test_empty_word_is_invalid(self):
+    # setup
         new_game = Game()
         # verify
         assert new_game.is_valid('') is False
@@ -45,7 +46,8 @@ class TestGame:
         assert new_game.is_valid(test_word) is False
         # teardown
         assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
-        def test_unknown_word_is_invalid(self):
+
+    def test_unknown_word_is_invalid(self):
         """A word that is not in the English dictionary should not be valid"""
         new_game = Game()
         new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
